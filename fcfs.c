8fe5bf9b8db6a4a45251eb73fbd37c502c1a4d59
samimshaikh11 <147469515+samimshaikh11@users.noreply.github.com>
2023-10-10 10:05:47 +0530

    Create fcfs.c

---
+++ fcfs.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{    
+    int number1, number2, sum;
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+    sum = number1 + number2;      
+    printf("The sum is ",sum);
+    return 0;
+}
